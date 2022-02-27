6c56171243041123a0e15c1cf4c19716f89888ee
Sourav Basu <hotelinventory2022@gmail.com>
2022-02-28 02:26:30 +0530

    added check for column existence

--- LookupTypeParentId_Add.sql
+++ LookupTypeParentId_Add.sql
@@ -1,12 +1,17 @@
-Alter table LookupType
-Add LookupTypeParentId int
+IF NOT EXISTS(SELECT 1 FROM sys.columns 
+          WHERE Name = N'LookupTypeParentId'
+          AND Object_ID = Object_ID(N'dbo.LookupType'))
+BEGIN	
+	Alter table LookupType
+	Add LookupTypeParentId int
+END
 GO
 
 ALTER view [dbo].[vw_LookupDetails]
 as
 select lt.LookupTypeParentId, lpt.LookupTypeName as LookupTypeParentName, lt.Id as LookupTypeId, lt.LookupTypeName as LookupType, l.Id as LookupId, l.LookupName as Lookup, l.Description as LookUpDescription
 from LookUpType lt
-join LookUpType lpt on lt.LookupTypeParentId = lpt.Id
 join LookUp l on lt.Id = l.LookupTypeId
+left join LookUpType lpt on lt.LookupTypeParentId = lpt.Id
 where lt.IsActive=1 and lt.IsDeleted=0 and l.IsActive=1 and l.IsDeleted=0
 GO
