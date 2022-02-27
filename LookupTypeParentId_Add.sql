cfdd5aee287192712a13e53ae17831288ad6f562
Sourav Basu <hotelinventory2022@gmail.com>
2022-02-28 02:23:23 +0530

    added script for lookuptypeparentname

--- LookupTypeParentId_Add.sql
+++ LookupTypeParentId_Add.sql
@@ -4,8 +4,9 @@ GO
 
 ALTER view [dbo].[vw_LookupDetails]
 as
-select lt.LookupTypeParentId, lt.Id as LookupTypeId, lt.LookupTypeName as LookupType, l.Id as LookupId, l.LookupName as Lookup, l.Description as LookUpDescription
+select lt.LookupTypeParentId, lpt.LookupTypeName as LookupTypeParentName, lt.Id as LookupTypeId, lt.LookupTypeName as LookupType, l.Id as LookupId, l.LookupName as Lookup, l.Description as LookUpDescription
 from LookUpType lt
+join LookUpType lpt on lt.LookupTypeParentId = lpt.Id
 join LookUp l on lt.Id = l.LookupTypeId
 where lt.IsActive=1 and lt.IsDeleted=0 and l.IsActive=1 and l.IsDeleted=0
 GO
